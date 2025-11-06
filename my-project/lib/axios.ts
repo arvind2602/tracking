8ad2562fcdf0f841a1b794901f5b7611a769c205
arvind2602 <guptaarvind2602@gmail.com>
2025-11-06 09:53:19 +0530

    update axios baseURL to use production endpoint

--- my-project/lib/axios.ts
+++ my-project/lib/axios.ts
@@ -3,7 +3,8 @@ import toast from 'react-hot-toast';
 
 const instance = axios.create({
   // baseURL: 'http://localhost:5000/api',
-  baseURL: 'https://89q8wp9g-5000.inc1.devtunnels.ms/api',
+  // baseURL: 'https://89q8wp9g-5000.inc1.devtunnels.ms/api',
+  baseURL: 'https://tasksb.vercel.app/api',
 });
 
 instance.interceptors.request.use((config) => {
