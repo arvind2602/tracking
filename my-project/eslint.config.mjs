75f3120ca15d50e413e8b7b08570d3117581684b
arvind2602 <guptaarvind2602@gmail.com>
2026-05-11 13:27:24 +0530

    Please provide the code or description of the changes so I can generate the commit message for you.

--- my-project/eslint.config.mjs
+++ my-project/eslint.config.mjs
@@ -11,6 +11,17 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-img-element": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react-hooks/set-state-in-effect": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
